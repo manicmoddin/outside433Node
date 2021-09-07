9fa17174ee67912288d14fab3773f1681430e5f6
manicmoddin <jimmy@manicmoddin.co.uk>
2021-09-07 21:17:42 +0100

    First commit

---
+++ outside433Node.ino
@@ -0,0 +1,15 @@
+///////////////////////////////////////////
+//                                       //
+// Outside node using JeeNode on 433 MHz //
+//                                       //
+///////////////////////////////////////////
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
